210a58e5661fa7613114df27ce20346619c107d0
in-serinder <97240804+in-serinder@users.noreply.github.com>
2023-05-31 13:09:40 +0800

    Add files via upload

---
+++ only_in.bat
@@ -0,0 +1,7 @@
+NetSh Advfirewall set allprofiles state on
+netsh advfirewall firewall add rule dir=in action=block name=stu_in program=C:\Windows\System32\Lenovo Teaching Systeam\student.exe
+netsh advfirewall firewall add rule dir=out action=block name=stu_out program=C:\Windows\System32\Lenovo Teaching Systeam\student.exe
+
+
+
+
