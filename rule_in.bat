28c235fa1fdbbd87d88d6b9b0ae5c8abeff27e5c
in-serinder <97240804+in-serinder@users.noreply.github.com>
2023-06-01 14:25:32 +0800

    Create rule_in.bat

---
+++ rule_in.bat
@@ -0,0 +1,3 @@
+NetSh Advfirewall set allprofiles state on
+netsh advfirewall firewall add rule dir=in action=block name=stu_in program=C:\Program Files (x86)\Lenovo teaching system\student.exe
+netsh advfirewall firewall add rule dir=out action=block name=stu_out program=C:\Program Files (x86)\Lenovo teaching system\student.exe
