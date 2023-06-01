5e0cd47fc007064bfd0e9d4e1d4bf31601669a2d
in-serinder <97240804+in-serinder@users.noreply.github.com>
2023-06-01 14:26:10 +0800

    Update only_in.bat

--- only_in.bat
+++ only_in.bat
@@ -1,7 +1,6 @@
 NetSh Advfirewall set allprofiles state on
-netsh advfirewall firewall add rule dir=in action=block name=stu_in program=C:\Windows\System32\Lenovo Teaching Systeam\student.exe
-netsh advfirewall firewall add rule dir=out action=block name=stu_out program=C:\Windows\System32\Lenovo Teaching Systeam\student.exe
-
+netsh advfirewall firewall add rule dir=in action=block name=stu_in program=C:\Program Files (x86)\Lenovo teaching system\student.exe
+netsh advfirewall firewall add rule dir=out action=block name=stu_out program=C:\Program Files (x86)\Lenovo teaching system\student.exe
 
 
 
