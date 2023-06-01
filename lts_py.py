ed19f0c71922952cddd4d70b76e8018945f0bd35
in-serinder <97240804+in-serinder@users.noreply.github.com>
2023-06-01 18:57:49 +0800

    Update lts_py.py

--- lts_py.py
+++ lts_py.py
@@ -6,8 +6,8 @@ ch=input("1：解除，2：恢复\n\n")
 if ch==1:
     os.system("NetSh Advfirewall set allprofiles state on")
     print("防火墙已开启")
-    os.system("netsh advfirewall firewall add rule dir=in action=block name=stu_in program=C:\Windows\System32\Lenovo Teaching Systeam\student.exe")
-    os.system("netsh advfirewall firewall add rule dir=out action=block name=stu_out program=C:\Windows\System32\Lenovo Teaching Systeam\student.exe")
+    os.system("netsh advfirewall firewall add rule dir=in action=block name=stu_in program=C:\Program Files (x86)\Lenovo teaching systeam\student.exe")
+    os.system("netsh advfirewall firewall add rule dir=out action=block name=stu_out program=C:\Program Files (x86)\Lenovo teaching systeam\student.exe")
     print("规则添加状态:\n\n","出栈状态:\n",os.system("netsh advfirewall firewall show rule=stu_in"),"入栈状态：\n",os.system("netsh advfirewall firewall show rule=stu_out"))
     print("已完成规则添加")
 else:
@@ -22,7 +22,7 @@ else:
     print("已经完成回滚")
 
 j=10
-for i in range(5):
+for i in range(10):
 
     print(j)
     time.sleep(1)
